a3b4946aac76669f2e1a0a44735f0a13aca6634e
Guy Brown <guy.brown@justchecking.co.uk>
2023-09-26 21:10:40 +0100

    cookie

--- src/app/info/cookie/page.jsx
+++ src/app/info/cookie/page.jsx
@@ -4,9 +4,58 @@ export default function Terms() {
   return (
     <Container>
 			<div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none space-y-4 mb-10">
-				<h2 className='text-lg font-bold'>Cookie Policy for dmbrokers.co.uk</h2>
+				<h2 className='text-lg font-bold'>Cookie Policy</h2>
 				<p className='text-sm'>Last updated: September 2023</p>
-				<p></p>
+				
+				<h3 className='font-semibold'>Does this website use cookies?</h3>
+				<p>Yes – this website uses cookies.</p>
+
+				<h3 className='font-semibold'>What are cookies?</h3>
+				<p>Cookies are small text files that are placed on the device you use to access the internet – such as a computer, phone or tablet – by websites that you visit. They are widely used in order to make websites work, or work more efficiently, as well as to provide information to website owners.</p>
+				<p>You can disable cookies by following the guidelines provided in this Cookie Policy but your experience on this website may be slightly impaired as a result.</p>
+
+
+				<h3 className='font-semibold'>What cookies are used on this website?</h3>
+				<p>The cookies we employ are used by most websites. This policy describes the types of cookies used on this website and explains their purpose.</p>
+
+				<h3 className='font-semibold'>Essential cookies</h3>
+				<p>Without these cookies, the basic features of this website cannot function. Essential cookies are required to enable you to navigate the website and use its standard features. We also use essential cookies to distinguish between humans and bots.</p>
+				
+				<h3 className='font-semibold'>Analytics cookies</h3>
+				<p>We use analytics cookies to track website behaviours, enabling us to understand how visitors access and use this website. Analytics reports provide us with information that can help us improve our website. Through using analytics cookies, we are able to monitor a number of behaviours which include:</p>
+				<ul className="list-disc ml-4">
+					<li>The number and frequency of website visitors</li>
+					<li>The length of time visitors spend browsing the website</li>
+					<li>Which types of device are used to access the website (mobile phones or tablets, for example)</li>
+					<li>Which pages are of most or least interest to our visitors</li>
+					<li>How visitors interact with our website’s pages.</li>
+				</ul>
+				<p>Analytics cookies do not collect any personally identifiable information, but we do collect IP addresses to help us monitor the approximate geographical locations of website users to help us better understand our visitor profile.</p>
+
+
+				<h3 className='font-semibold'>Advertising cookies</h3>
+				<p>Advertising cookies are used to deliver online ads that are deemed relevant to our visitors and their interests. They are also used to limit the number of times advertisements are displayed as well as help measure the effectiveness of digital marketing campaigns. Advertising cookies are placed by advertising networks when website owners choose to run digital marketing campaigns and can track which websites users visit. Whilst advertising campaigns are active, you may be served ads on other websites or social media platforms after having visited this website, for example.</p>
+
+				
+
+
+				<h3 className='font-semibold'>How do I manage my cookie settings?</h3>
+				<p>Commonly used web browsers allow you to manage most cookies via user settings. Find out how to manage cookies on popular browsers:</p>
+				<ul>
+					<li><a href="https://support.google.com/accounts/answer/61416?co=GENIE.Platform%3DDesktop&hl=en" target="_blank" className="cursor-pointer underline">Google Chrome</a></li>
+					<li><a href="https://support.microsoft.com/en-us/windows/microsoft-edge-browsing-data-and-privacy-bb8174ba-9d73-dcf2-9b4a-c582b4e640dd" target="_blank" className="cursor-pointer underline">Microsoft Edge</a></li>
+					<li><a href="https://support.mozilla.org/en-US/kb/enhanced-tracking-protection-firefox-desktop?redirectslug=enable-and-disable-cookies-website-preferences&redirectlocale=en-US" target="_blank" className="cursor-pointer underline">Mozilla Firefox</a></li>
+					<li><a href="https://support.microsoft.com/en-gb/windows/delete-and-manage-cookies-168dab11-0753-043d-7c16-ede5947fc64d" target="_blank" className="cursor-pointer underline">Microsoft Internet Explorer</a></li>
+					<li><a href="https://help.opera.com/en/latest/web-preferences/" target="_blank" className="cursor-pointer underline">Opera</a></li>
+					<li><a href="https://support.apple.com/en-gb/safari" target="_blank" className="cursor-pointer underline">Apple Safari</a></li>
+				</ul>
+				<p>To find information relating to other browsers, visit the browser developer’s website.</p>
+				<p>To opt out of Google Analytics tracking visit <a href="https://tools.google.com/dlpage/gaoptout" target="_blank" className="cursor-pointer underline">tools.google.com/dlpage/gaoptout</a>.</p>
+
+				<h3 className='font-semibold'>Where can I learn more about cookies?</h3>
+				<p>To find out more about cookies, including how to see what cookies have been set, visit <a href="https://www.aboutcookies.org/" target="_blank" className="cursor-pointer underline">aboutcookies.org</a> or <a href="https://allaboutcookies.org/" target="_blank" className="cursor-pointer underline">allaboutcookies.org</a>.</p>
+
+
 			</div>
 			
 		</Container>
