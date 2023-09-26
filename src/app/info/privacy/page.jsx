c385159630869fbf9d20a899bffb506ac6956921
Guy Brown <guy.brown@justchecking.co.uk>
2023-09-26 19:34:06 +0100

    infor container and styles

--- src/app/info/privacy/page.jsx
+++ src/app/info/privacy/page.jsx
@@ -1,7 +1,16 @@
+import { Container } from "@/components/Container";
+
 export default function Privacy() {
   return (
-    <>
-      <h1>Privacy</h1>
-    </>
+
+		<Container>
+			<div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none space-y-4">
+				<h2 className='text-lg font-bold'>Privacy Policy for dmbrokers.co.uk</h2>
+				<p>Last updated: September 2023</p>
+				<p></p>
+			</div>
+			
+		</Container>
+
   )
 }
--- src/app/info/terms/page.jsx
+++ src/app/info/terms/page.jsx
@@ -1,7 +1,14 @@
+import { Container } from "@/components/Container";
+
 export default function Terms() {
   return (
-    <>
-      <h1>Terms</h1>
-    </>
+    <Container>
+			<div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none space-y-4">
+				<h2 className='text-lg font-bold'>Terms and Conditions for dmbrokers.co.uk</h2>
+				<p>Last updated: September 2023</p>
+				<p></p>
+			</div>
+			
+		</Container>
   )
 }
