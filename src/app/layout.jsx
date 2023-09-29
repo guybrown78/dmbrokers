da86eb4c9536f77272fa487d39eb36e74a21e6f9
Guy Brown <guy.brown@justchecking.co.uk>
2023-09-29 14:33:08 +0100

    google analytics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,8 @@
 import { Sumana, Trirong, Noto_Serif } from 'next/font/google'
 
 import clsx from 'clsx'
-
+import GoogleAnalytics from '@/components/GoogleAnalytics';
+import CookieBanner from '@/components/CookieBanner';
 import '@/styles/tailwind.css'
 
 const sumana = Sumana({
@@ -38,8 +39,12 @@ export default function RootLayout({ children }) {
       lang="en"
       className={clsx('h-full scroll-smooth bg-gray-50 antialiased', notoSerif.variable)}
     >
+			<GoogleAnalytics GA_MEASUREMENT_ID='G-GHH2B4MSGM' />
       <body className="flex h-full flex-col">
-        <div className="flex min-h-full flex-col">{children}</div>
+        <div className="flex min-h-full flex-col">
+					{children}
+					<CookieBanner />
+				</div>
       </body>
     </html>
   )
